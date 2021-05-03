15870bf95bcc45d5fd3b0f1c27fe16ef4a17e0ed
118140008 Laila Noer Islami Hidayatullah <80301658+laila118140008@users.noreply.github.com>
2021-05-03 10:36:26 +0700

    Update Tentangapp.js

--- src/pages/Tentangapp.js
+++ src/pages/Tentangapp.js
@@ -22,3 +22,21 @@ const styles = StyleSheet.create({
     
   }
 });
+
+const displayAnImage = () =>{
+  return (
+    <View style={styles.container}>
+      <Image 
+        style={styles.logo}
+        source={{
+          uri:
+          'https://www.bing.com/images/blob?bcid=S3Brg9A8rbICeQ',
+        }}
+      />
+      <Text style={styles.text}>Aplikasi E-Raport v1.6</Text>
+      <Text style={styles.text}>Kurikulum 2013</Text>
+    </View>
+  );
+}
+
+export default displayAnImage;
