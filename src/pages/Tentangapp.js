be22247d88a99698d286f767b40ad1d18f706bfb
118140008 Laila Noer Islami Hidayatullah <80301658+laila118140008@users.noreply.github.com>
2021-05-03 10:36:04 +0700

    Create Tentangapp.js

---
+++ src/pages/Tentangapp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 50,
+  },
+  logo: {
+   width: 260,
+   height: 96,
+   marginVertical: 40, 
+  },
+  text:{
+    justifyContent: "center",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: 5,
+    width: 300,
+    
+  }
+});
